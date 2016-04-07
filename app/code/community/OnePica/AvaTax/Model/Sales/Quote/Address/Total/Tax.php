dc55a71d3143bee2954f24df073a87cad334d14c
Oleksandr Marynych <o.marynych@astoundcommerce.com>
2016-04-07 16:37:17 +0300

    Implemented AVTNG-820: implement Discounts FPT in AvaTax plugin refactoring...

--- app/code/community/OnePica/AvaTax/Model/Sales/Quote/Address/Total/Tax.php
+++ app/code/community/OnePica/AvaTax/Model/Sales/Quote/Address/Total/Tax.php
@@ -545,17 +545,13 @@ class OnePica_AvaTax_Model_Sales_Quote_Address_Total_Tax extends Mage_Sales_Mode
 
     protected function _applayFPTDiscount(Mage_Sales_Model_Quote_Address $address)
     {
-        $verMageSalesRule = Mage::getConfig()->getNode()->modules->Mage_SalesRule->version;
-        if (version_compare($verMageSalesRule, '1.6.0.3') >= 0) {
-        }
-
         $quote = $address->getQuote();
         $store = $quote->getStore();
         if (Mage::helper('weee')->isEnabled() && Mage::helper('weee')->isDiscounted($store)) {
             /* @var $calc Mage_SalesRule_Model_Validator */
             $calc = Mage::getModel('salesrule/validator');
             // processWeeeAmount method was implemented only in magento 1.8.0.0, Mage_SalesRule 1.6.0.3
-            // earlier versions of magento, like 1.7.0.0 does not support discount per FPT,
+            // earlier versions of magento, like 1.7.0.0 does not support discount per FPT in totals,
             // only whole discount on subtotal
             if (method_exists($calc, 'processWeeeAmount')) {
                 $calc->init(
@@ -566,6 +562,8 @@ class OnePica_AvaTax_Model_Sales_Quote_Address_Total_Tax extends Mage_Sales_Mode
                 $calc->processWeeeAmount($address, $items);
             }
         }
+
+        return $this;
     }
 
     /**
